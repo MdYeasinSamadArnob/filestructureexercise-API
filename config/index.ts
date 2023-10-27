ad039b5e3606d70a50bd8066425e9deb6ce7a613
MdYeasinSamadArnob <yeasinsamadarnob@gmail.com>
2023-10-28 00:11:28 +0600

    Api ready 5

--- config/index.ts
+++ config/index.ts
@@ -5,5 +5,5 @@ dotenv.config({ path: path.join(process.cwd(), ".env") });
 export default {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
-  database_url: process.env.DATABASE_URL,
+  database_url: 'mongodb+srv://yeasinsamadarnob:Klufuv8XWoce70ye@cluster0.wr1wfuo.mongodb.net/?retryWrites=true&w=majority',
 };
